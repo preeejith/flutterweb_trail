3f8d5ba06e9e892bea0719985f1be3698cb561e1
ShinuFrancis2020 <shinufrancis@mca.ajce.in>
2023-03-02 10:05:45 +0530

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,55 +14,58 @@ class _HomeScreenState extends State<HomeScreen> {
       drawer: const Drawer(),
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: const Text("Demo Website"),
         elevation: 0,
         actions: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(right: 8),
             child: Row(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.min,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Home",
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Services",
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Events",
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Gallery",
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Testimonials",
                     style: TextStyle(
                         color: Colors.black, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(12.0),
+                const Padding(
+                  padding: EdgeInsets.only(right: 8),
                   child: Text(
                     "Contacts",
                     style: TextStyle(
@@ -72,6 +75,113 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Home",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Services",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Events",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Gallery",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Testimonials",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(12.0),
+          //   child: Text(
+          //     "Contacts",
+          //     style:
+          //         TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          //   ),
+          // ),
+
+          // Flexible(
+          //   child: Container(
+          //     width: MediaQuery.of(context).size.width,
+          //     child: Row(
+          //       // ignore: prefer_const_literals_to_create_immutables
+          //       children: [
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Home",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Services",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Events",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Gallery",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Testimonials",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //         const Padding(
+          //           padding: EdgeInsets.all(12.0),
+          //           child: Text(
+          //             "Contacts",
+          //             style: TextStyle(
+          //                 color: Colors.black, fontWeight: FontWeight.bold),
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
         ],
       ),
       body: SingleChildScrollView(
@@ -154,56 +264,170 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ]),
             Padding(
-              padding: const EdgeInsets.only(top: 10.0),
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 25,
+              ),
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 19.0),
-                  child: Column(children: [
-                    Row(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 25,
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            const Text(
-                              "About Me",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  letterSpacing: .5),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 10.0, left: 88, right: 20),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(12),
-                                child: Container(
-                                  height: 200,
-                                  color: Colors.grey.shade200,
-                                  child: const Image(
-                                    image: AssetImage("assets/profile.png"),
+                            Flexible(
+                              child: Column(
+                                children: [
+                                  const Text(
+                                    "About Me",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 25,
+                                        letterSpacing: .5),
                                   ),
-                                ),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                        top:
+                                            MediaQuery.of(context).size.height /
+                                                25,
+                                        left:
+                                            MediaQuery.of(context).size.height /
+                                                6,
+                                        right:
+                                            MediaQuery.of(context).size.height /
+                                                10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(12),
+                                      child: Container(
+                                        height: 200,
+                                        color: Colors.grey.shade200,
+                                        child: const Image(
+                                          image:
+                                              AssetImage("assets/profile.png"),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 1.5,
+                              child: const Text(
+                                "Hello Everyone! To start, I would like to share that I am a complete  Animal Lover and passionate about Wlidlife Conservation! Beyond, a thorough and competent Administrative and Executive Business Support Specialist, having worked across multiple domains and industries from the Private SectorCentral & State Govt. of India, Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration, C-Suite/Executive Business Support, Consulting, Human Resources, Recruitment Practices, Employee Engagement & Experience, Central & State Govt. of India, Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration, C-Suite/Executive Business Support, Consulting, Human Resources, Recruitment Practices, Employee Engagement & Experience,",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13,
+                                    letterSpacing: .2),
                               ),
                             ),
                           ],
-                        ),
+                        )
+                      ]),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height / 1,
+                    width: MediaQuery.of(context).size.width,
+                    color: const Color(0xff16308C),
+                    child: Column(
+                      children: [
                         SizedBox(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          child: const Text(
-                            "Hello Everyone! To start, I would like to share that I am a complete  Animal Lover and passionate about Wlidlife Conservation! Beyond, a thorough and competent Administrative and Executive Business Support Specialist, having worked across multiple domains and industries from the Private SectorCentral & State Govt. of India, Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration, C-Suite/Executive Business Support, Consulting, Human Resources, Recruitment Practices, Employee Engagement & Experience, Central & State Govt. of India, Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration, C-Suite/Executive Business Support, Consulting, Human Resources, Recruitment Practices, Employee Engagement & Experience,",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 13,
-                                letterSpacing: .2),
-                          ),
+                          height: 50,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    const CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        radius: 25,
+                                        backgroundImage:
+                                            AssetImage("assets/profile.png")),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "Education",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                // Text("ggg"),
+                                Padding(
+                                  padding: const EdgeInsets.all(55.0),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    width:
+                                        MediaQuery.of(context).size.width / 2,
+                                    child: const Text(
+                                        "Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration,"),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Column(
+                              // ignore: prefer_const_literals_to_create_immutables
+                              children: [
+                                Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    const CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        radius: 25,
+                                        backgroundImage:
+                                            AssetImage("assets/profile.png")),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        "Education",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                // Text("ggg"),
+                                Padding(
+                                  padding: const EdgeInsets.all(55.0),
+                                  child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width / 2,
+                                    child: Text(
+                                        "Higher Education & Fine Arts Institutes, Manufacturing & Service Industries, Executive Search Firms, Product Companies, and Non-Profit Organizations, accomplishing wide-ranging and specialized functions in Office Administration,"),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
                         ),
                       ],
-                    )
-                  ]),
-                ),
+                    ),
+                  ),
+                ],
               ),
             )
           ],
